f40a1dbcaf04abb754bf17ff77eb09975faae8dc
Richard Paul Bäck <ritschmaster@t1000.home>
2016-06-14 09:36:21 +0200

    Fixes some bugs in the SMTP setup. But there are still some!

--- lisp/init-gnus.el
+++ lisp/init-gnus.el
@@ -74,33 +74,42 @@
 
 (defun set-smtp (mech server port user password)
   "Set related SMTP variables for supplied parameters."
-  (setq smtpmail-smtp-server server
+  (setq smtpmail-default-smtp-server server
+        smtpmail-smtp-server server
         smtpmail-smtp-service port
         smtpmail-auth-credentials (list (list server port user
                                               password))
         smtpmail-auth-supported (list mech)
-        smtpmail-starttls-credentials nil)
+        smtpmail-starttls-credentials nil
+        smtpmail-smtp-user user
+        smtpmail-user-mail-address user
+        user-mail-address user)
   (message "Setting SMTP server to `%s:%s' for user `%s'."
            server port user))
 
 (defun set-smtp-ssl (server port user password &optional key
                             cert)
   "Set related SMTP and SSL variables for supplied parameters."
-  (setq starttls-use-gnutls t
-        starttls-gnutls-program "gnutls-cli"
-        starttls-extra-arguments nil
-        smtpmail-smtp-server server
-        smtpmail-smtp-service port
-        smtpmail-auth-credentials (list (list server port user
-                                              password))
-        smtpmail-starttls-credentials (list (list
-                                             server port key cert)))
+  (setq ;; starttls-use-gnutls t
+   ;; starttls-gnutls-program "gnutls-cli"
+   ;; starttls-extra-arguments nil
+   smtpmail-stream-type 'starttls
+   smtpmail-default-smtp-server server
+   smtpmail-smtp-server server
+   smtpmail-smtp-service port
+   smtpmail-auth-credentials (list (list server port user
+                                         password))
+   smtpmail-starttls-credentials (list (list
+                                        server port key cert))
+   smtpmail-smtp-user user
+   smtpmail-user-mail-address user
+   user-mail-address user)
   (message
    "Setting SMTP server to `%s:%s' for user `%s'. (SSL
 enabled.)" server port user))
 
-; This function will complain if you fill the from field with
-; an account not present in smtp-accounts.
+                                        ; This function will complain if you fill the from field with
+                                        ; an account not present in smtp-accounts.
 (defun change-smtp ()
   "Change the SMTP server according to the current from line."
   (save-excursion
@@ -187,8 +196,8 @@ information."))))
   :group 'init-gnus)
 
 ;; Automatic signing/encryption if possible
-(setq
- message-send-hook
+(add-hook
+ 'message-send-hook
  '((lambda ()
      (cond ((message-mail-p)
             (let ((toheader (message-fetch-field "To")))
