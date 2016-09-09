1849e65f61bd887d4a530ec0e1eb99d584f9c000
Richard Paul Bäck <richard.baeck@free-your-pc.com>
2016-09-09 18:33:23 +0200

    Some minor fixes.

--- lisp/init-erc.el
+++ lisp/init-erc.el
@@ -3,10 +3,13 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'erc)
 (setq erc-prompt-for-password t)
+(setq erc-current-nick-highlight-type 'all)
 (defun start-irc ()
   "This allows the connection to an IRC server which is already specified. It uses the ~/.authinfo file."
   (interactive)
   (erc-tls))
+
+(require-package 'znc)
 
 (provide 'init-erc)
 ;;; init-erc ends here
